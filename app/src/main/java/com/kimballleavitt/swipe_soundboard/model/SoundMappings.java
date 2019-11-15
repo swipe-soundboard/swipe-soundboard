62be836470dc5e27991e7cd7cc506ad007dbd247
Lance Haderlie <lance.haderlie@gmail.com>
2019-11-15 13:59:22 -0700

    added config functionality and removal of mappings

--- app/src/main/java/com/kimballleavitt/swipe_soundboard/model/SoundMappings.java
+++ app/src/main/java/com/kimballleavitt/swipe_soundboard/model/SoundMappings.java
@@ -3,18 +3,38 @@ package com.kimballleavitt.swipe_soundboard.model;
 import android.net.Uri;
 
 import com.andrognito.patternlockview.PatternLockView;
+import com.google.gson.reflect.TypeToken;
 import com.kimballleavitt.swipe_soundboard.exception.MappingExistsException;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.Serializable;
+import java.io.Writer;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import com.google.gson.*;
 
 public class SoundMappings {
     private static SoundMappings soundMappings = new SoundMappings();
+    private Gson serializer;
 
     private SoundMappings() {
+        serializer = new Gson();
+        Type patternSoundMapType = new TypeToken<Map<StoragePattern, Uri>>() {}.getType();
+        File config = new File("config.json");
+        if(config.exists() && !config.isDirectory()) {
+            try {
+               String text = new Scanner(config).useDelimiter("\\A").next();
+               this.patternsToSounds = serializer.fromJson(text, patternSoundMapType);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static SoundMappings getInstance() {
@@ -45,7 +65,14 @@ public class SoundMappings {
 
     public void removeMapping(List<PatternLockView.Dot> pattern) {
         try {
-            // TODO
+            StoragePattern toRemove = new StoragePattern(pattern);
+            if(patternsToSounds.containsKey(toRemove)) {
+                patternsToSounds.remove(toRemove);
+            }
+            else {
+                System.out.println("That pattern isn't mapped to anything!");
+                return;
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -65,6 +92,23 @@ public class SoundMappings {
             throw new MappingExistsException("Pattern exists", existingUri);
         }
         patternsToSounds.put(new StoragePattern(pattern), fileUri);
+    }
+
+    public void saveMappings() {
+        if(serializer == null){
+            serializer = new Gson();
+        }
+        String saveConfig = serializer.toJson(patternsToSounds);
+        File config = new File("config.json");
+        try {
+            Writer writer = new FileWriter(config);
+            writer.write(saveConfig);
+            writer.close();
+        }
+        catch(Exception e){
+            System.out.println("Something went wrong in the saving of the config file");
+            e.printStackTrace();
+        }
     }
 
     public class StoragePattern implements Serializable {
